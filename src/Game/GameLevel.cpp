b37860ba279a8d063825073cbf27b47a9ebba6f3
AntonioRasulo <antonio.rasuloa@libero.it>
2026-08-25 11:06:09 +0100

    GameLevel class implemented Load function

--- src/Game/GameLevel.cpp
+++ src/Game/GameLevel.cpp
@@ -1,5 +1,6 @@
 #include "GameLevel.hpp"
 #include <fstream>
+#include <sstream>
 
 namespace game {
 
@@ -20,8 +21,39 @@ namespace game {
             // Read each line from level file
             while(std::getline(fstream, line))
             {
-
+                std::istringstream sstream(line);
+                std::vector<unsigned int> row;
+                int xCoord = -1;
+                // Read each word separated by spaces
+                while(sstream >> tileCode)
+                {
+                    if(xCoord == -1)
+                    {
+                        xCoord = tileCode;
+                    }
+                    else
+                    {
+                        std::pair<int, int> coordPair(xCoord, tileCode);
+                        tileData.push_back(coordPair);
+                        xCoord = -1;
+                    }
+                }
+                if(tileData.empty() == false)
+                {
+                    Init(tileData);
+                }
             }
+        }
+    }
+
+    void GameLevel::Init(std::vector<std::pair<int, int>> tileData)
+    {
+        //unsigned int lvlWidth =
+        // const int NUM_COLUMN = 50;
+        // const int NUM_ROWS = 50;
+        for(auto coords : tileData)
+        {
+            
         }
     }
 
